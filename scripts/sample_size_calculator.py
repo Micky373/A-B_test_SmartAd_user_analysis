1a9265903a0ade4f081000ebbb815edcad6d417c
Michael Tamirie <michaeltamirie@gmail.com>
2022-05-18 23:34:33 +0300

    Increasing readability and modularity

---
+++ scripts/sample_size_calculator.py
@@ -0,0 +1,12 @@
+import scipy.stats as scs
+
+# calculate the sample size needed for the specified power and confidence level
+
+
+def min_sample_size(bcr, mde, power=0.8, sig_level=0.05):
+    standard_norm = scs.norm(0, 1)
+    beta = standard_norm.ppf(power)
+    alpha = standard_norm.ppf(1-sig_level/2)
+    pooled_prob = (bcr + bcr+mde) / 2
+    min_N = (2 * pooled_prob * (1 - pooled_prob) * (beta + alpha)**2/ mde**2)
+    return min_N
